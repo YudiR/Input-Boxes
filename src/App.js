408dd25a9455e6ac4d496ee066e144d70bd5897e
YudiR <yudrose13@gmail.com>
2019-08-07 11:15:46 -0400

    characters typed into input gets displayed below. On click the character is deleted

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { text: [], stringText: null };
+    
+    // this.ref = ''
+  }
+
+  textChange = eventText => {
+    // this.state.push(eventText.split(/(?!$)/u))
+
+    this.setState({ text: [...eventText.split("")] });
+    this.setState({stringText: eventText})
+    console.log(this.state.text, "array!!!");
+  };
+
+deleteLetter = (letter,i) => {
+var array = [...this.state.text]
+array.splice(i,1)
+this.setState({ text: array})
+this.setState({stringText: array.join("")})
+// this.refs.someName.value = ''
+
+console.log('hey',i)
+}
+
+  render() {
+    this.setState();
+    return (
+      <div className="App">
+
+      
+        <input
+          value = {this.state.stringText} 
+          onChange={event => {
+            this.textChange(event.target.value);
+          }}
+          ref="someName"
+        />
+<div class = "break"></div>
+        {this.state.text.map((item, i) => (
+          
+          <h1
+            onClick={(event) => this.deleteLetter(event.target.value,i)}
+            id="h1"
+            key={i}
+            style={{
+              border: "2px solid black"
+            }}
+          >
+            {item}
+          </h1>
+          
+        ))}
+        {/* <h1> {this.state.text} </h1>  */}
+        {console.log(this.state.text, "state!!")}
+      </div>
+    );
+  }
 }
 
 export default App;
